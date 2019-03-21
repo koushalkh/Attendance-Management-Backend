f44a328e47d9f16e1fd6b9094bdb6f2cd1061f5d
ChetanBM <chetanbanavikalmutt1997@gmail.com>
2019-03-21 19:40:18 +0530

    Registered models in management app

--- management/admin.py
+++ management/admin.py
@@ -1,3 +1,17 @@
+"""
+Register models in management app
+"""
 from django.contrib import admin
+from .models import Teacher, Period, TimeSlot, \
+    Subject, StudentSubject, Student, CollegeClass, Attendance
 
 # Register your models here.
+
+admin.site.register(Teacher)
+admin.site.register(Period)
+admin.site.register(TimeSlot)
+admin.site.register(Subject)
+admin.site.register(StudentSubject)
+admin.site.register(Student)
+admin.site.register(CollegeClass)
+admin.site.register(Attendance)
